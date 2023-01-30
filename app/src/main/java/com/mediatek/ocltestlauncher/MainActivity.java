b7346e962a01ed9959e9bc93f1c7e81a381e0e6d
RuinedStar <arthurx94@gmail.com>
2023-01-31 00:05:29 +0800

    First commit default pure project

---
+++ app/src/main/java/com/mediatek/ocltestlauncher/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mediatek.ocltestlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
